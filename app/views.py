eabd13fab1bd3ce0076ceb1b67b69759529bbf7d
Diyorbek <majidovdiyorbek741@gmail.com>
2023-04-08 13:56:39 +0500

    add car

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Car, Konfigurator
+from .serializers import CarSerializer, KonfiguratorSerializer
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class CarViewSet(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = CarSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.data)
