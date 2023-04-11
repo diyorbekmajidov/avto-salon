647c4ab2482f6b5a589692528d9942f597416cef
Diyorbek <majidovdiyorbek741@gmail.com>
2023-04-11 17:29:49 +0500

    1

--- app/serializers.py
+++ app/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from .models import Car, Konfigurator
 
-class CarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Car
-        fields = '__all__'
+
     
 class KonfiguratorSerializer(serializers.ModelSerializer):
-    sub_car = CarSerializer(many=True, read_only=True)
     class Meta:
         model = Konfigurator
+        fields = '__all__'
+
+class CarSerializer(serializers.ModelSerializer):
+    sub_car = KonfiguratorSerializer(many=True, read_only=True)
+    class Meta:
+        model = Car
         fields = '__all__'
