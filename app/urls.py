4a5616a2932f9c92ca6857eb55ef2b75022d0e5c
Diyorbek <majidovdiyorbek741@gmail.com>
2023-04-13 13:04:33 +0500

    konfigurutor id

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CarViewSet, KonfiguratorViewSet
+from .views import CarViewSet, KonfiguratorViewSet,Konfiguratorget
 
 urlpatterns = [
     path('car/', CarViewSet.as_view()),
     path('car/<int:pk>/', CarViewSet.as_view()),
     path('konfigurator/', KonfiguratorViewSet.as_view()),
-    path('konfigurator/<int:pk>/', KonfiguratorViewSet.as_view()),
+    path('konfigurator/<int:pk>/', Konfiguratorget.as_view()),
 ]
