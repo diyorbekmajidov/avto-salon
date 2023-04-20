de56e21881bb46210cb4c84f17fc2746a0870ab3
Diyorbek <majidovdiyorbek741@gmail.com>
2023-04-20 13:17:41 +0500

    1

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from .views import (CarViewSet,
     KonfiguratorViewSet,
     Konfiguratorget,
     CarViewdelete,
+    Konfiguratordelete,
     )
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('konfigurator/', KonfiguratorViewSet.as_view()),
     path('konfigurator/<int:pk>/', Konfiguratorget.as_view()),
     path('car/delete/<int:pk>/', CarViewdelete.as_view()),
+    path('konfigurator/delete/<int:pk>/', Konfiguratordelete.as_view()),
 ]
