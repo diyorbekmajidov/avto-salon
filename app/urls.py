39db684dc1e0a205d452b1c8a4f7ee18356586eb
Diyorbek <majidovdiyorbek741@gmail.com>
2023-04-20 13:15:32 +0500

    1

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import CarViewSet, KonfiguratorViewSet,Konfiguratorget
+from .views import (CarViewSet, 
+    KonfiguratorViewSet,
+    Konfiguratorget,
+    CarViewdelete,
+    )
 
 urlpatterns = [
     path('car/', CarViewSet.as_view()),
     path('car/<int:pk>/', CarViewSet.as_view()),
     path('konfigurator/', KonfiguratorViewSet.as_view()),
     path('konfigurator/<int:pk>/', Konfiguratorget.as_view()),
+    path('car/delete/<int:pk>/', CarViewdelete.as_view()),
 ]
