5bc35d88ffdf35c37e008ba0a30176adc3579577
Diyorbek <majidovdiyorbek741@gmail.com>
2023-05-07 15:24:41 +0500

    get by id konfigurator

--- app/urls.py
+++ app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('car/', CarViewSet.as_view()),
     path('car/<int:pk>/', CarViewSet.as_view()),
     path('konfigurator/', KonfiguratorViewSet.as_view()),
-    path('konfigurator/<int:pk>/', Konfiguratorget.as_view()),
+    path('konfiguratorget/<int:pk>/', Konfiguratorget.as_view()),
     path('car/delete/<int:pk>/', CarViewdelete.as_view()),
     path('konfigurator/delete/<int:pk>/', Konfiguratordelete.as_view()),
     path('dilery/', DileryViewSet.as_view()),
