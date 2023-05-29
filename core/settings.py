11a36671f0222c9fc85938981fe8c96ec21f89f9
Diyorbek <majidovdiyorbek741@gmail.com>
2023-05-29 14:13:53 +0500

    static root

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'rest_framework',
-    'ckeditor',
     'rest_framework.authtoken',
     'corsheaders',
 ]
@@ -49,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     "corsheaders.middleware.CorsMiddleware",
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
