4b437d9b0b77048e52453381abe79f3455b825cf
Diyorbek <majidovdiyorbek741@gmail.com>
2023-05-29 14:37:06 +0500

    static root

--- core/settings.py
+++ core/settings.py
@@ -124,6 +124,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
